0e6d08af5a13c44d6a30a28f4bb67862bafe9183
Federico Dotti <fede.dotti.94@gmail.com>
2025-10-17 19:01:22 +0000

    Add deletion dialog

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const myLibrary = [];
 
-function Book() {
-
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }  
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.bookId = crypto.randomUUID();
+  this.info = function() {
+    const isRead = read ? "read" : "not read yet";
+    return title + " by " + author + `, ${pages} pages, ` + isRead;
+  };
 }
 
 function addBookToLibrary() {
